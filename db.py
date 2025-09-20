ce6df1601ff30c02ce099e38a21fa7a0ca71417e
jevontrei <160815182+jevontrei@users.noreply.github.com>
2025-09-21 09:59:50 +1000

    add maps and place-adding etc

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import sqlite3
 
+
 def init_db():
     con = sqlite3.connect("maps.db")
-    
+
     # create tables
-    con.execute('''
+    con.execute("""
                 create table if not exists places (
                     id integer primary key autoincrement,
                     name text not null,
@@ -12,23 +13,25 @@ def init_db():
                     lat real,
                     long real
                 )
-                ''')
-    
+                """)
+
     # seed
-    con.execute('''
+    con.execute("""
                 insert or ignore into places (
                     name, country, lat, long
                 )
                 values 
                 ('Brisbane', 'Australia', -27.47, 153.03),
                 ('Bali', 'Indonesia', -8.41, 115.19),
-                ('Bangkok', 'Thailand', 13.76, 100.50)
-                ''')
+                ('Bangkok', 'Thailand', 13.76, 100.50),
+                ('Negombo', 'Sri Lanka', 7.21, 79.85),
+                ('Hong Kong', 'Hong Kong', 22.29, 114.12)
+                """)
 
     con.commit()  # write changes to file/disk
     con.close()
-    
+
+
 if __name__ == "__main__":
     init_db()
     print("db is initted!")
-    
